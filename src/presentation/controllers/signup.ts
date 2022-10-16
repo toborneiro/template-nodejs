6cc5670b62241ec3fbc03bf0a58bc558db033e79
Marcio Braga <marciobragamg@gmail.com>
2022-10-16 19:47:13 -0300

    refactor: remove if else complexity

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 export class SignUpController {
   handle(httpResquest: HttpRequest): HttpResponse {
-    if (!httpResquest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
-    if (!httpResquest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    const requiredField = ['name', 'email', 'password', 'passwordConfirmation'];
+    for (const field of requiredField) {
+      if (!httpResquest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }
