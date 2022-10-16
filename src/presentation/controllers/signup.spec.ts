7218acdf219a104dc935da92cf4d0773de2c58b5
Marcio Braga <marciobragamg@gmail.com>
2022-10-16 17:44:50 -0300

    test: ensure signupController return 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup';
+
+describe('Signup Controller', () => {
+  test('Should Return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpResquest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpResquest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
