3708be5a9f056fd0d4b87375a1e8326bd37bb30a
Marcio Braga <marciobragamg@gmail.com>
2022-10-16 22:46:48 -0300

    test: ensure signup returns 500 if addaccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -199,3 +199,23 @@ describe('Signup Controller', () => {
     });
   });
 });
+
+describe('Signup Controller', () => {
+  test('Should Return 500 if addAccount throws', () => {
+    const { sut, addAccountStub } = makeSut();
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpResquest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+});
